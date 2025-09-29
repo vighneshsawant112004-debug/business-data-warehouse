9dd160fbd8af721409b508446658959f11d911c5
vighneshsawant112004-debug <vighneshsawant112004@gmail.com>
2025-09-29 20:28:49 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,13 +1,13 @@
-*/
-===============================================================================
-DDL Script: Create Silver Tables
-===============================================================================
-Script Purpose:
-    This script creates tables in the 'silver' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
-*/
+
+# ===============================================================================
+# DDL Script: Create Silver Tables
+# ===============================================================================
+# Script Purpose:
+   # This script creates tables in the 'silver' schema, dropping existing tables 
+   # if they already exist.
+	#  Run this script to re-define the DDL structure of 'bronze' Tables
+# ===============================================================================
+
 
 # ==============================================================================
 # Build Silver Layer
